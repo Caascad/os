6807c8aa0bb6349e403f2b207b88bcabd98818b4
Jean-Philippe Braun <eon@patapon.info>
2020-06-06 11:18:17 +0200

    Misc improvements
    * wrap openstackclient and vault
    * allow to override VAULT_ADDR
    * allow to use domain scoped secrets
    * exit on errors
    * reindent, retab with spaces

--- default.nix
+++ default.nix
@@ -1,17 +1,24 @@
 { sources ? import ./nix/sources.nix
 , nixpkgs ? sources.nixpkgs
+, toolbox ? sources.toolbox
 , pkgs ? import nixpkgs {}
+, tbox ? import toolbox {}
 }:
 
 with pkgs;
+with pkgs.lib;
 
 stdenv.mkDerivation rec {
   pname = "os";
   version = "1.0.0";
 
   unpackPhase = ":";
+
+  buildInputs = [ makeWrapper ];
+
   installPhase = ''
     install -m755 -D ${./os} $out/bin/os
+    wrapProgram $out/bin/os --prefix PATH ":" ${with tbox; makeBinPath [ openstackclient vault ]}
   '';
 
   meta = with stdenv.lib; {
